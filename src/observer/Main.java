1015b83da5f07c2cc9f62ea9a0c043fc6d95a5b0
ehu <ehu@158.227.124.140>
2020-11-03 12:16:32 +0100

    Bigarren atala eginda,semaforoan gauza bat falta da

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -10,6 +10,16 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		Observable pacient=new Covid19Pacient("Melvin",25);
+		Observable pacient1=new Covid19Pacient("Juan", 65);
+		PacientObserverGUI a=new PacientObserverGUI(pacient);
+		PacientSymptomGUI frame=new PacientSymptomGUI((Covid19Pacient) pacient);
+		PacientThermometerGUI frameT=new PacientThermometerGUI(pacient);
+		SemaphorGUI frameS=new SemaphorGUI(pacient);
+		
+//		PacientObserverGUI b=new PacientObserverGUI(pacient1);
+//		PacientSymptomGUI frame1=new PacientSymptomGUI((Covid19Pacient) pacient1);
+//		PacientThermometerGUI frameT2=new PacientThermometerGUI(pacient1);
 
 	}
 
